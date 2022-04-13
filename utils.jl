a4ddbce04690acb9a1681e5bb6fa6e09457b4489
Oscar Smith <oscardssmith@gmail.com>
2022-04-13 08:52:19 -0400

    make times diff correctly.

--- utils.jl
+++ utils.jl
@@ -5,16 +5,16 @@ macro gctime(ex)
     quote
         $fc
         n = isempty(ARGS) ? 1 : parse(Int, ARGS[1])
-        local times = Vector{Float64}(undef, n)
+        local times = Vector{Float64}(undef, n+1)
         local vals = Vector{Any}(undef, n)
         local stats = Base.gc_num()
-        t0 = time_ns()
-        for i in 1:n
+        times[1] = time_ns()
+        for i in 2:n+1
             vals[i] = $(esc(ex))
             times[i] = time_ns()
         end
         local diff = Base.GC_Diff(Base.gc_num(), stats)
-        @. times = (times - t0) / 1e9
+        times = diff(times) ./ 1e9
         result = (value=vals, times=times, bytes=diff.allocd, gctime=diff.total_time/(1e9*n), gcstats=diff)
         ARGS[end] == "SERIALIZE" ? serialize(stdout, result) : display(result)
     end
