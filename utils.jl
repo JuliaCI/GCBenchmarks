e4bc965e31851a780477535ef381f7bf03fbbc92
Christine H. Flood <christineflood@juliacomputing.com>
2022-04-15 13:34:35 -0400

    Timings need to take into account that the runs are cummulative.

--- utils.jl
+++ utils.jl
@@ -9,12 +9,13 @@ macro gctime(ex)
         n = isempty(ARGS) ? 1 : parse(Int, ARGS[1])
         local times = Vector{Float64}(undef, n)
         local vals = Vector{Any}(undef, n)
-        local stats = Vector{Base.GC_Num}(undef, n)
+        local stats = Vector{Base.GC_Diff}(undef, n)
         for i in 1:n
-            start = time_ns()
+            start_time = time_ns()
+            start_gc_num = Base.gc_num()
             vals[i] = $(esc(ex))
-            times[i] = time_ns() - start
-            stats[i] = Base.gc_num()
+            times[i] = time_ns() - start_time
+            stats[i] = Base.GC_Diff(Base.gc_num(), start_gc_num)
         end
         result = (value=vals, times=times, stasts=stats)
         ARGS[end] == "SERIALIZE" ? serialize(stdout, result) : display(result)
