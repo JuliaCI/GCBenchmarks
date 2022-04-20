bf977e2a1e32e9d015860b4b78b469356c8fbec5
Oscar Smith <oscardssmith@gmail.com>
2022-04-20 17:50:10 -0400

    remove loop part 2

--- run_benchmarks.jl
+++ run_benchmarks.jl
@@ -12,9 +12,17 @@ for category in readdir()
     for test in readdir()
         endswith(test, ".jl") || continue
         @show test
-        result = open(deserialize, `$JULIAVER --project=. $test $RUNS SERIALIZE`)
-        #result=(value, times, bytes, gctime, gcstats)
-        println("median time: ", median(result.times))
+        result = (value=[], time=[], bytes=[], gctime=[], gcstats=[])
+        for _ in 1:RUNS
+            r = open(deserialize, `$JULIAVER --project=. $test SERIALIZE`)
+            push!(result.value,   r.value)
+            push!(result.time,    r.time)
+            push!(result.bytes,   r.bytes)
+            push!(result.gctime,  r.gctime)
+            push!(result.gcstats, r.gcstats)
+        end
+        #result=(value, time, bytes, gctime, gcstats)
+        println("median time: ", median(result.time))
         println("mean GC time: ", result.gctime)
     end
     cd("..")
