a6603f26ed8ebea394a39e318f2aa223aed0b60e
Oscar Smith <oscardssmith@gmail.com>
2022-04-20 17:40:05 -0400

    Update utils.jl

--- utils.jl
+++ utils.jl
@@ -1,25 +1,18 @@
 using Serialization
 
-
-
 macro gctime(ex)
     fc = isdefined(Base.Experimental, Symbol("@force_compile")) ? :(Base.Experimental.@force_compile) : :()
     quote
         $fc
-        n = isempty(ARGS) ? 1 : parse(Int, ARGS[1])
-        local times = Vector{Float64}(undef, n)
-        local vals = Vector{Any}(undef, n)
-        local stats = Vector{Base.GC_Diff}(undef, n)
-        local gc_num = Vector{Base.GC_Num}(undef, n)
+        local stats = Base.gc_num()
+        t0 = time_ns()
         for i in 1:n
-            start_time = time_ns()
-            start_gc_num = Base.gc_num()
             vals[i] = $(esc(ex))
-            times[i] = time_ns() - start_time
-            stats[i] = Base.GC_Diff(Base.gc_num(), start_gc_num)
-            gc_num[i] = Base.gc_num()
+            times[i] = time_ns()
         end
-        result = (value=vals, times=times, stats=stats, gc_num=gc_num)
+        local diff = Base.GC_Diff(Base.gc_num(), stats)
+        @. times = (times - t0) / 1e9
+        result = (value=vals, times=times, bytes=diff.allocd, gctime=diff.total_time/(1e9*n), gcstats=diff)
         ARGS[end] == "SERIALIZE" ? serialize(stdout, result) : display(result)
     end
 end
