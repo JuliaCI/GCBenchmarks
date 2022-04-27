909d57dea2c2a46a765a44eb44d0f26212b48839
Oscar Smith <oscardssmith@gmail.com>
2022-04-27 10:52:54 -0400

    update run_one to host the loop

--- run_one_benchmark.jl
+++ run_one_benchmark.jl
@@ -16,12 +16,19 @@ dir = joinpath(@__DIR__, path)
 cd(dir)
 @printf("Running Benchmark path = %s file = %s times = %d\n", path, file, RUNS)
 @show BENCH
-result = open(deserialize, `$JULIAVER --project=. $file $RUNS SERIALIZE`)
-(value,times,stats)= result
+value=[]
+times=[]
+stats=[]
+for _ in 1:RUNS
+   r = open(deserialize, `$JULIAVER --project=. $test SERIALIZE`)
+   push!(value, r.value)
+   push!(times, r.times)
+   push!(stats, r.stats)
+end
 @printf("run time: %0.0fms min, %0.0fms max %0.0fms median\n",
-   minimum(result.times) / 1_000_000,
-   maximum(result.times) / 1_000_000,
-   median(result.times)  / 1_000_000)
+   minimum(times) / 1_000_000,
+   maximum(times) / 1_000_000,
+   median(times)  / 1_000_000)
 
 time = map(gctime, stats)
 @printf("gc time: %0.0fms min, %0.0fms max, %0.0fms median\n",
