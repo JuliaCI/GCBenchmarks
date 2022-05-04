1a804da629516f21bd29a09f4b140c61e9156927
oscarddssmith <oscar.smith@juliacomputing.com>
2022-05-04 16:13:15 -0400

    use argparse

--- run_benchmarks.jl
+++ run_benchmarks.jl
@@ -1,42 +1,83 @@
-# Usage:
-#   julia run_benchmarks.jl [<#runs> <#threads>]
-
 using Statistics
 using Serialization
 using Printf
+using ArgParse
 
 const RUNS = isempty(ARGS) ? 10 : parse(Int, ARGS[1])
-const THREADS = isempty(ARGS) ? 1 : parse(Int, ARGS[2])
 const JULIAVER = Base.julia_cmd()[1]
-dir = joinpath(@__DIR__, "benches")
-cd(dir)
+
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table! s begin
+        "--runs", "-n"
+            help = "number of threads"
+            arg_type = Int
+            default = 10
+        "--threads", "-t"
+            help = "another option with an argument"
+            arg_type = Int
+            default = 1
+        "--bench"
+            help = "if specified, runs a single benchmark"
+            arg_type = String
+            default = "all"
+    end
+
+    return parse_args(s)
+end
 
 gctime(stat) = stat.total_time
 
-for category in readdir()
-    @show category
-    cd(category)
-    for test in readdir()
-        endswith(test, ".jl") || continue
-        @show test
-        value = []
-        times = []
-        stats = []
-        for _ in 1:RUNS
-            r = open(deserialize, `$JULIAVER --project=. --threads=$THREADS $test SERIALIZE`)
-            push!(value, r.value)
-            push!(times, r.times)
-            push!(stats, r.stats)
+function run_one_bench(runs, threads, file)
+    value = []
+    times = []
+    stats = []
+    for _ in 1:runs
+        r = open(deserialize, `$JULIAVER --project=. --threads=$threads $file SERIALIZE`)
+        push!(value, r.value)
+        push!(times, r.times)
+        push!(stats, r.stats)
+    end
+    @printf("run time: %0.0fms min, %0.0fms max %0.0fms median\n",
+       minimum(times) / 1_000_000,
+       maximum(times) / 1_000_000,
+       median(times) / 1_000_000)
+    time = map(gctime, stats)
+    @printf("gc time: %0.0fms min, %0.0fms max, %0.0fms median\n",
+       minimum(time) / 1_000_000,
+       maximum(time) / 1_000_000,
+       median(time) / 1_000_000)
+end
+
+function run_all_benches(runs, threads)
+    for category in readdir()
+        @show category
+        cd(category)
+        for file in readdir()
+            @show file
+            endswith(file, ".jl") || continue
+            run_one_bench(runs, threads, file)
         end
-        @printf("run time: %0.0fms min, %0.0fms max %0.0fms median\n",
-           minimum(times) / 1_000_000,
-           maximum(times) / 1_000_000,
-           median(times)  / 1_000_000)
-        time = map(gctime, stats)
-        @printf("gc time: %0.0fms min, %0.0fms max, %0.0fms median\n",
-           minimum(time) / 1_000_000,
-           maximum(time) / 1_000_000,
-           median(time)  / 1_000_000)
     end
     cd("..")
 end
+
+function main()
+    args = parse_commandline()
+    JULIAVER = Base.julia_cmd()[1]
+    bench, runs, threads = args["bench"], args["runs"], args["threads"]
+
+    dir = joinpath(@__DIR__, "benches")
+    cd(dir)
+    if bench == "all"
+        run_all_benches(runs, threads)
+    else
+        path, file = splitpath(bench)
+        cd(path)
+        run_one_bench(runs, threads, file)
+    end
+end
+
+main()
+
