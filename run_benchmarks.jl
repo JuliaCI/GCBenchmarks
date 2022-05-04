38f9ee4c7269adef89db38bd12ab238da9e65f31
K Pamnany <kpamnany@users.noreply.github.com>
2022-05-04 15:32:27 -0400

    Add a command-line option to specify #threads
    Also a little cleanup.

--- run_benchmarks.jl
+++ run_benchmarks.jl
@@ -1,8 +1,12 @@
+# Usage:
+#   julia run_benchmarks.jl [<#runs> <#threads>]
+
 using Statistics
 using Serialization
 using Printf
 
 const RUNS = isempty(ARGS) ? 10 : parse(Int, ARGS[1])
+const THREADS = isempty(ARGS) ? 1 : parse(Int, ARGS[2])
 const JULIAVER = Base.julia_cmd()[1]
 dir = joinpath(@__DIR__, "benches")
 cd(dir)
@@ -15,25 +19,24 @@ for category in readdir()
     for test in readdir()
         endswith(test, ".jl") || continue
         @show test
-        value=[]
-        times=[]
-        stats=[]
+        value = []
+        times = []
+        stats = []
         for _ in 1:RUNS
-            r = open(deserialize, `$JULIAVER --project=. $test SERIALIZE`)
+            r = open(deserialize, `$JULIAVER --project=. --threads=$THREADS $test SERIALIZE`)
             push!(value, r.value)
             push!(times, r.times)
             push!(stats, r.stats)
         end
         @printf("run time: %0.0fms min, %0.0fms max %0.0fms median\n",
-           minimum(result.times)/ 1_000_000,
-           maximum(result.times)/ 1_000_000,
-           median(result.times) / 1_000_000)
+           minimum(times) / 1_000_000,
+           maximum(times) / 1_000_000,
+           median(times)  / 1_000_000)
         time = map(gctime, stats)
         @printf("gc time: %0.0fms min, %0.0fms max, %0.0fms median\n",
-           minimum(time)/ 1_000_000,
-           maximum(time)/ 1_000_000,
-           median(time) / 1_000_000)
+           minimum(time) / 1_000_000,
+           maximum(time) / 1_000_000,
+           median(time)  / 1_000_000)
     end
     cd("..")
 end
-
