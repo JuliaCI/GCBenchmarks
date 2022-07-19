76b59ce1bd966aaad7f221f86b59f8a9a87225a5
Valentin Churavy <vchuravy@users.noreply.github.com>
2022-07-19 12:29:20 -0400

    Benchmark array of objects (#38)

---
+++ benches/serial/obj_arrays/single_ref.jl
@@ -0,0 +1,28 @@
+include(joinpath("..", "..", "..", "utils.jl"))
+
+module SingleRef
+
+"""
+This benchmark stresses the array handling in the GC.
+We allocate a large arrays that all contain a reference to a singular object.
+The mark-queue of the GC should not overflow.
+"""
+function construct(array_length)
+    obj = Ref{Int}(0)
+    arr = Array{Ref{Int}}(undef, array_length)
+    fill!(arr, obj)
+    GC.gc(true)
+    GC.gc(true)
+    Core.donotdelete(arr)
+    return nothing
+end
+
+end #module
+
+using .SingleRef
+
+const MAX_MEMORY = round(Int, 0.8 * Sys.total_memory())
+const array_length = div(MAX_MEMORY, sizeof(Ptr{C_NULL}))
+
+@info "SingleRef bench" array_length MAX_MEMORY
+@gctime SingleRef.construct(array_length)
