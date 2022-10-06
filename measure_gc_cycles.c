fb2ac114265988fc937e88c6162d65df5b10ffc6
Diogo Netto <dcn@Diogos-MacBook-Air.local>
2022-10-06 16:41:39 -0400

    started perf stuff

---
+++ measure_gc_cycles.c
@@ -0,0 +1,41 @@
+// Based on `https://man7.org/linux/man-pages/man2/perf_event_open.2.html`
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <linux/perf_event.h>
+#include <asm/unistd.h>
+
+long perf_event_start(void)
+{
+   struct perf_event_attr pe;
+   memset(&pe, 0, sizeof(pe));
+   pe.type = PERF_TYPE_HARDWARE;
+   pe.size = sizeof(pe);
+   pe.config = PERF_COUNT_HW_INSTRUCTIONS;
+   pe.disabled = 1;
+   pe.exclude_kernel = 1;
+   pe.exclude_hv = 1;
+
+   int fd = syscall(__NR_perf_event_open, &pe, 0, -1, -1, 0);
+   if (fd == -1) {
+      fprintf(stderr, "Error opening leader\n", pe.config);
+      exit(1);
+   }
+
+   ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+   ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
+
+   return fd;
+}
+
+long perf_event_end(int fd)
+{
+   long long count;
+   ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+   read(fd, &count, sizeof(count));
+   close(fd);
+   return count;
+}
--- utils.jl
+++ utils.jl
@@ -2,11 +2,28 @@ using Pkg
 Pkg.instantiate() # It is dumb that I have to do this
 using Serialization
 
+const perf_fd = Ref(UInt64(0))
+const cycles_in_gc = Ref(UInt64(0))
+
+function gc_cb_pre()
+    perf_fd[] = ccall((:perf_event_start, "lib_gc_benchmarks.so"), Cvoid, ())
+    nothing
+end
+
+function gc_cb_post()
+    cycles_in_gc[] += ccall((:perf_event_start, "lib_gc_benchmarks.so"), Clonglong, (Cint,), (perf_fd[],))
+    nothing
+end
+
 macro gctime(ex)
     fc = isdefined(Base.Experimental, Symbol("@force_compile")) ?
         :(Base.Experimental.@force_compile) :
         :()
     quote
+        ccall(:jl_gc_set_cb_pre_gc, Cvoid, (Ptr{Cvoid}, Cint),
+              @cfunction(gc_cb_pre, Cvoid, (Cint,)), true)
+        ccall(:jl_gc_set_cb_post_gc, Cvoid, (Ptr{Cvoid}, Cint),
+              @cfunction(gc_cb_post, Cvoid, (Cint,)), true)
         $fc
         local result
         try
@@ -15,11 +32,13 @@ macro gctime(ex)
             local val = $(esc(ex))
             local end_time = time_ns()
             local end_gc_num = Base.gc_num()
+            local cycles_in_gc = cycles_in_gc[]
             result = (
                 value = val,
                 times = (end_time - start_time),
                 gc_diff = Base.GC_Diff(end_gc_num, start_gc_num),
-                gc_end = end_gc_num
+                gc_end = end_gc_num,
+                cycles_in_gc = cycles_in_gc,
             )
         catch e
             @show e
