46475e45523fc3370ed89c5a20f2d1dfb6711759
d-netto <diogonetto.dcn@gmail.com>
2024-01-19 22:21:35 -0300

    add MWE from JuliaLang 52937 as a benchmark

---
+++ benches/serial/obj_arrays/issue-52937.jl
@@ -0,0 +1,34 @@
+include(joinpath("..", "..", "..", "utils.jl"))
+
+using BenchmarkTools
+
+using Base.Threads: @threads
+using Random: shuffle
+
+function sample_vote!(_rb, chop_counts)
+    pts = rand(length(chop_counts))
+
+    N = length(_rb)
+    _srt = 4245
+    partialsortperm!(_rb, pts, 1:_srt; lt = <, rev = true)
+    while sum(@views chop_counts[_rb[1:_srt]]) ≤ 5660
+        _srt = min(2 * _srt, N)
+        partialsortperm!(_rb, pts, 1:_srt; lt = <, rev = true)
+    end
+end
+
+function parallel_scores(chop_counts)
+    for k ∈ 1:8 # @threads in real code, but not needed for mwe. do need -t8 though
+        _rb = collect(1:length(chop_counts))
+
+        # the bigger this number, the more % GC time
+        for _ ∈ 1:1000
+            sample_vote!(_rb, chop_counts)
+        end
+    end
+end
+
+# kind of arbitrary, but approximates my data
+chop_counts = shuffle(trunc.(Int, 6500 ./ (50:100_000)))
+
+@gctime parallel_scores(chop_counts)
