c7482e0a83f9513e619674d1f64d51769bf4fa7f
Diogo Netto <61364108+d-netto@users.noreply.github.com>
2024-02-19 16:04:28 -0300

    increase amount of garbage allocated in TimeZones (#88)

--- benches/serial/TimeZones/TimeZones.jl
+++ benches/serial/TimeZones/TimeZones.jl
@@ -4,5 +4,5 @@ using TimeZones
 
 zdts = [now(tz"UTC") for _ in  1:100_000_000];
 
-@gctime sum(hash, ["trashfire"^min(1000, i) for i in 1:100_000])
+@gctime sum(hash, ["trashfire"^min(1000, i) for i in 1:500_000])
 
